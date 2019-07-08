14205b9890b3b0b5e12764e2760a1dff5b7203f5
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2019-07-08 08:24:53 -0400

    First Try ready

--- FirstTry/app.R
+++ FirstTry/app.R
@@ -1,49 +1,103 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(raster)
+library(rgdal)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(
+    ui = tagList(
+        navbarPage("",
+                   
+                   tabPanel("DogImages",
+                            fluidRow(
+                                column(width = 6,
+                                       uiOutput('mytabs')
+                                ) )
+                            
+                   )#cerrado tab panel 2
+                   
+        ) #cerrado de navbar Panel    
+        
+    ), #cerrado ui
+    
+    server <- function(input, output) {
+        
+    
+        #####################################
+        # Tabs breeds
+        
+        output$mytabs = renderUI({
+            Species <- c(1:4)
+            Random <- sample(Species, 4)
+            myTabs = lapply(Random, function(i){
+                
+                
+                tabPanel(paste("Breed", i),
+                         
+                         ###
+                         fluidPage(
+                             fluidRow(
+                                 column(6,
+                                        wellPanel(
+                                            plotOutput(paste0("PlotBreed",i)))),
+                                 renderUI({
+                                     Habitats <- c(1:3)
+                                     RandomH <- sample(Habitats, 3)
+                                     Values <- reactiveValues(j = 1)
+                                     output$Numb <- renderText(Values$j)
+                                     observe({
+                                         input$New_Button
+                                         isolate(Values$j <- Values$j + 1)
+                                     })
+                                     
+                                     
+                                     
+                                     column(6,actionButton("New_Button", "Next house"),
+                                            wellPanel(
+                                                plotOutput(paste0("PlotHouse",RandomH[1]))),
+                                            textOutput("Numb")
+                                     )
+                                     
+                                 })
+                             )
+                         ))
+                ####
+            })
+            
+            
+            do.call(tabsetPanel, myTabs)
+        })
+        
+        output$PlotBreed1 <- renderPlot({
+            a <- stack("Images/border_collie.jpg")
+            plotRGB(a)
+        })
+        
+        output$PlotBreed2 <- renderPlot({
+            b <- stack("Images/German-Shepherd.jpg")
+            plotRGB(b)
+        })
+        
+        output$PlotBreed3 <- renderPlot({
+            c <- stack("Images/Yorkshire.jpg")
+            plotRGB(c)
+        })  
+    
+        output$PlotHouse1 <- renderPlot({
+            d <- stack("Images/Appartment.jpeg")
+            plotRGB(d)
+        })
+        
+        output$PlotHouse2 <- renderPlot({
+            e <- stack("Images/House.jpg")
+            plotRGB(e)
+        })
+        
+        output$PlotHouse3 <- renderPlot({
+            f <- stack("Images/cottage.jpg")
+            plotRGB(f)
+        })
+        
+        
+    }
+    
+) #cerrado de shiny
