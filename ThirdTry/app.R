f6b91a4ea3cb28d0f044d34531e77e994992a896
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2019-07-14 19:07:46 -0400

    Third is the charm

--- ThirdTry/app.R
+++ ThirdTry/app.R
@@ -1,49 +1,93 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+Habitats <- c(1:3)
+RandomH <- sample(Habitats, 3)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(
+    ui = tagList(
+        navbarPage("",
+                   
+                   tabPanel("DogImages",
+                            fluidRow(
+                                column(width = 6,
+                                       uiOutput('mytabs')
+                                ) )
+                            
+                   )#cerrado tab panel 2
+                   
+        ) #cerrado de navbar Panel    
+        
+    ), #cerrado ui
+    
+    server <- function(input, output) {
+        
+        
+        global <- reactiveValues(nr = 1)    
+        
+        observeEvent(input$whichTab, {
+            global$nr <- 1 
+        })
+        
+        observeEvent(input$New_Button1, {
+            global$nr <- min(global$nr + 1, max(RandomH))
+        })
+        
+        observeEvent(input$New_Button2, {
+            global$nr <- min(global$nr + 1, max(RandomH))
+        })
+        
+        observeEvent(input$New_Button3, {
+            global$nr <- min(global$nr + 1, max(RandomH))
+        })
+        
+        
+        #####################################
+        # Tabs breeds
+        
+        output$mytabs = renderUI({
+            Breeds <- c(1:3)
+            Random <- sample(Breeds, 3)
+            myTabs = lapply(Random, function(i){
+                
+                
+                tabPanel(paste("Breed", i),
+                         
+                         ###
+                         fluidPage(
+                             fluidRow(
+                                 column(6,
+                                        wellPanel(
+                                            img(src = paste0("Dog",i, ".jpg"), height = 300, width = 300))),
+                                 renderUI({
+                                     
+                                     column(6, actionButton(inputId = paste0("New_Button", i), "Next house"),
+                                            wellPanel(
+                                                uiOutput(paste0("PlotHouse", i)))
+                                     )
+                                     
+                                 })
+                             )
+                         ))
+                ####
+            })
+            
+            
+            do.call(tabsetPanel, c(id = "whichTab", myTabs))
+        })
+        
+        output$PlotHouse1 <- renderUI({
+            img(src = paste0("House", RandomH[global$nr],".jpg"), height = 300, width = 300)
+        })
+        
+        output$PlotHouse2 <- renderUI({
+            img(src = paste0("House", RandomH[global$nr],".jpg"), height = 300, width = 300)
+        })
+        
+        output$PlotHouse3 <- renderUI({
+            img(src = paste0("House", RandomH[global$nr],".jpg"), height = 300, width = 300)
+        })
+        
+        
+    }
+    
+) #cerrado de shiny
